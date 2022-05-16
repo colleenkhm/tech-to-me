06ec1b285b08602873edfb23a2e35cdf4ecb61c3
Colleen Murray <colleenkhm@gmail.com>
2022-05-15 20:49:23 -0500

    deleted entryComment model and associations, added index.js

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Entry = require('./Entry');
+
+const User = require('./User');
+
+const Comment = require('./Comment');
+
+// Entry belongs to User
+Entry.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// User will have many Entries
+User.hasMany(Entry, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Entry has many comments
+Entry.hasMany(Comment, {
+    foreignKey: 'entry',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {
+    Entry,
+    User,
+    Comment,
+};
